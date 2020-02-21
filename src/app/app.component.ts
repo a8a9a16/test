25a12ca52c8e41d187a95cac699d13f8d653bb44
Muqing <leemuqing@gmail.com>
2020-02-21 17:28:48 -0500

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 /*********************************************************************************
- * *  WEB422 – Assignment 05
+ * *  WEB422 – Assignment 06
  * *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
  * *  assignment has been copied manually or electronically from any other source (including web sites) or 
  * *  distributed to other students.
  * * 
- * *  Name: ___Muqing Li______ Student ID: _014151138____ Date: __2019/07/19___
+ * *  Name: ___Muqing Li______ Student ID: _014151138____ Date: __2019/08/02___
  * *
  * ********************************************************************************/ 
 
